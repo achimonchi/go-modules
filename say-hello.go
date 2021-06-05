5b409cb7fa21bb840c2e46400b0f15d362aa97fc
Reyhan Jovie <achimonchi.reyhan@gmail.com>
2021-06-05 22:58:22 +0700

    add 2 parameter in sayhello

--- say-hello.go
+++ say-hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello(name string) string{
-	return  "Hello " + name
+func SayHello(name string, age string) string{
+	return  "Hello " + name + " My Age is " + age
 }
