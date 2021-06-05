cd7fa5c7932711ad5ff4f0b4e4c74f47c71f7679
Reyhan Jovie <achimonchi.reyhan@gmail.com>
2021-06-05 23:06:01 +0700

    change dir

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	go_say_hello "github.com/achimonchi/go-modules/v2/mod"
+)
+
+func main(){
+	fmt.Println(go_say_hello.SayHello("Reyha", "10"))
+}
--- mod/say-hello.go
+++ mod/say-hello.go
@@ -1,4 +1,4 @@
-package go_say_hello
+package mod
 
 func SayHello(name string, age string) string{
 	return  "Hello " + name + " My Age is " + age
